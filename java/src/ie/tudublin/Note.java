ff6a4b180e92357fe208d4b539a150584313380d
John Quinless <john.quinless@gmail.com>
2021-04-21 16:17:34 +0100

    Created Constructor and toString Method

--- java/src/ie/tudublin/Note.java
+++ java/src/ie/tudublin/Note.java
@@ -3,4 +3,17 @@ package ie.tudublin;
 public class Note {
     char note;
     int duration;
+
+    public Note(){
+        note = 'A';
+        duration = 2;
+    }
+
+    public String toString()
+    {
+        return this.note + " " + this.duration + " ";
+    }
 }
+
+
+
