ce35cf1b8beb071f280d742f25233da8652fd4fd
John Quinless <john.quinless@gmail.com>
2021-04-21 17:16:24 +0100

    Added Arraylist for Notes in ScoreDisplays and attempted loadScore and printScores methods

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -2,12 +2,46 @@ package ie.tudublin;
 
 import java.util.ArrayList;
 
+import java.util.List;
+
+import java.util.Arrays;
 
 import processing.core.PApplet;
 
 public class ScoreDisplay extends PApplet
 {
-	
+	ArrayList<Note> notes = new ArrayList<Note>();
+
+	private void loadScore() 
+	{
+		StringBuilder sb = new StringBuilder();
+
+		for (char c: score.toCharArray()) {
+   			sb.append(c).append(" ");
+		}
+
+		System.out.println(sb.toString().trim());
+
+		String str[] = score.split(" ");
+
+		notes = Arrays.asList(str);
+		for (String s: notes){
+			System.out.println(s);
+		}
+	}
+
+	private void printScores()
+	{
+		for (int i = 0; i < notes.size(); i++)
+		{
+			String display = notes.get(i);
+
+			String tabbeddisplay = display.substring(0)+'\t'+display.substring(1)+'\t'+display.substring(2, display.length());
+
+			System.out.println(tabbeddisplay);
+		}
+	}
+
 	String score = "DEFGABcd";
 	//String score = "D2E2F2G2A2B2c2d2";
 	//String score = "DEF2F2F2EFA2A2B2AFD2E2D2D2D2";
@@ -24,7 +58,8 @@ public class ScoreDisplay extends PApplet
 
 	public void setup() 
 	{
-		
+		loadScore();
+		printScores();
 	}
 
 	public void draw()
